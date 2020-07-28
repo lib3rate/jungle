97437cb8e85c1a0db83f642d2f50e8315492fe4f
Dmitry Petrov <e.liberate@gmail.com>
2020-07-28 04:11:13 +0000

    Added field validations for the User model

--- spec/models/product_spec_spec.rb
+++ spec/models/product_spec_spec.rb
@@ -10,33 +10,29 @@ RSpec.describe Product, type: :model do
   }
 
   describe 'Validations' do
-    it "should create a product" do
-      expect(subject).to be_a Product
-    end
-
     it "creates a product when the required fields are filled" do
       expect(subject).to be_valid
     end
 
-    it "does not create a product when the name is not provided" do
+    it "does not create a product if the name is not provided" do
       subject.name = nil
       expect(subject).to_not be_valid
       expect(subject.errors.full_messages).to include "Name can't be blank"
     end
 
-    it "does not create a product when the price is not provided" do
+    it "does not create a product if the price is not provided" do
       subject.price_cents = nil
       expect(subject).to_not be_valid
       expect(subject.errors.full_messages).to include "Price can't be blank"
     end
 
-    it "does not create a product when the quantity is not provided" do
+    it "does not create a product if the quantity is not provided" do
       subject.quantity = nil
       expect(subject).to_not be_valid
       expect(subject.errors.full_messages).to include "Quantity can't be blank"
     end
 
-    it "does not create a product when the category is not provided" do
+    it "does not create a product if the category is not provided" do
       subject.category = nil
       expect(subject).to_not be_valid
       expect(subject.errors.full_messages).to include "Category can't be blank"
---
+++ spec/models/user_spec_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject {
+    described_class.create( first_name: "Joe",
+                            last_name: "Black",
+                            email: "test@example.com",
+                            password: "test_password",
+                            password_confirmation: "test_password")
+  }
+
+  describe 'Validations' do
+    it "creates a user when the required fields are filled" do
+      expect(subject).to be_valid
+    end
+
+    it "validates that email, first name, last name, password, and password confirmation are present" do
+      expect(subject.first_name).to be_present
+      expect(subject.last_name).to be_present
+      expect(subject.email).to be_present
+      expect(subject.password).to be_present
+      expect(subject.password_confirmation).to be_present
+    end
+
+    it "does not create a user if the password and password confirmation do not match" do
+      subject.password_confirmation = "different_password"
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include "Password confirmation doesn't match Password"
+    end
+    
+    it "does not create a user if the email already exists in the database" do
+      first_user = User.create( first_name: "Joe",
+                                last_name: "Black",
+                                email: "test@test.COM",
+                                password: "test_password",
+                                password_confirmation: "test_password")
+      second_user = User.create(first_name: "Scruff",
+                                last_name: "McGruff",
+                                email: "TEST@TEST.com",
+                                password: "test_password",
+                                password_confirmation: "test_password")
+      expect(second_user).to_not be_valid
+      # expect(second_user.save).to be false
+      # puts second_user.errors.full_messages
+      expect(second_user.errors.full_messages).to include "Email has already been taken"
+    end
+    
+    # it "does not create a user if the password is too short" do
+    #   subject.password = "1"
+    #   subject.password_confirmation = "1"
+    #   # expect(subject).to_not be_valid
+    #   expect(subject.save).to be false
+    #   puts subject.errors.full_messages
+    #   # expect(subject.errors.full_messages).to include "Quantity can't be blank"
+    # end
+
+  #   it "does not create a product when the category is not provided" do
+  #     subject.category = nil
+  #     expect(subject).to_not be_valid
+  #     expect(subject.errors.full_messages).to include "Category can't be blank"
+  #   end
+  end
+end
